d2593e223d0eff8599e19e11eeb9b53474d0bb7a
Vishal Patil <vishalpatilvgp2199@gmail.com>
2026-03-02 11:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-//your JS code here. If required.
 const input = document.getElementById("ip");
 const button = document.getElementById("btn");
 const output = document.getElementById("output");
 
-button.addEventlistener("click", function (){
-	const value = Number(input.value);
+button.addEventListener("click", function () {
 
-	new Promise(function (resolve) {
-		setTimeout(function () {
-			output.textContent = "result: " + value;
-			resolve(value);
-		}, 2000);
-	})
+  const value = Number(input.value);
 
-	.then(function (num) {
-		return new Promise(function (resolve) {
-			setTimeout(function (){
-				const result = num * 2;
-				output.textContent = "Result: " + result;
-				resolve(result);
-			}, 1000);
-		});
-	})
-	.then(function (num) {
-		return new Promise(function () {
-			const result = num - 3;
-			output.textContent = "Result: " + result;
-			resolve(result);
-		}, 1000);
-	});
-})
+  new Promise(function (resolve) {
+    setTimeout(function () {
+      output.textContent = "Result: " + value;
+      resolve(value);
+    }, 2000);
+  })
 
-.then(function (num) {
-	return new Promise(function (resolve) {
-		setTimeout(function () {
-			const result = num / 2;
-			output.textContent = "Result: " + result;
-			resolve(result);
-		}, 1000);
-	});
-})
-.then(function (num) {
-	return new Promise(function (resolve) {
-		setTimeout(function () {
-			const finalResult = num + 10;
-			output.textContent = "Final Result: " + finalResult;
-			resolve(finalResult);
-		}, 1000);
-	});
+  .then(function (num) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        const result = num * 2;
+        output.textContent = "Result: " + result;
+        resolve(result);
+      }, 1000);
+    });
+  })
+
+  .then(function (num) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        const result = num - 3;
+        output.textContent = "Result: " + result;
+        resolve(result);
+      }, 1000);
+    });
+  })
+
+  .then(function (num) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        const result = num / 2;
+        output.textContent = "Result: " + result;
+        resolve(result);
+      }, 1000);
+    });
+  })
+
+  .then(function (num) {
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        const finalResult = num + 10;
+        output.textContent = "Final Result: " + finalResult;
+        resolve(finalResult);
+      }, 1000);
+    });
+  });
+
 });
